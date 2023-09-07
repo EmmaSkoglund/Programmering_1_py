80e8270e60d33bf822cd3aef2c4031b4dd504e3e
EmmaSkoglund <emma_skoglund@outlook.com>
2023-09-07 16:08:53 +0200

    Färdigställd uppfift, kapitel 7

--- Kapitel_7/Uppgift_7.1.py
+++ Kapitel_7/Uppgift_7.1.py
@@ -7,11 +7,13 @@ POST_3 = ''
 
 while True:
 
+    # Rensar terminalfönster
     if os.name == 'nt':
         os.system('cls')
-    elif os.name == 'posix':
-        os.system('clear')
+    else:
+            os.system('clear')
 
+    # Skriver ut instruktioner
     print('.: basicBILLBOARD :.')
     print('*' * ui_bredd)
     print('P1:', POST_1)
@@ -21,10 +23,13 @@ while True:
     print('c  Ändra post')
     print('e  Stäng program')
 
+    # Hämtar komando från användaren
     meny = input('meny > ')
 
+    # Utför opperationer för inmatat kommando
     if meny == 'c':
         id = input('id > ')
+
         if id == '1':
             meddelande = input('meddelande > ')
             POST_1 = meddelande
@@ -37,18 +42,19 @@ while True:
             meddelande = input('meddelande > ')
             POST_3 = meddelande
 
+        elif id != '1, 2, 3':
+            print(f'- FEL: Felaktigt ID ')
+            print('- INFO: Vänligen ge heltel mellan 1-3')
+            print('-' * ui_bredd)
+            input('Tryck enter för att fortsätta...')
+
     elif meny == 'e':
         break
 
     if meny != 'c':
         print(f'- FEL: okänt kommando ({meny})')
         print('-' * ui_bredd)
-        print('Tryck enter för att fortsätta...')
+        input('Tryck enter för att fortsätta...')
 
-    if id != ['1', '2', '3']:
-        print(f'- FEL: Felaktigt ID ')
-        print('- INFO: Vänligen ge heltel mellan 1-3')
-        print('-' * ui_bredd)
-        print('Tryck enter för att fortsätta...')
 
 
