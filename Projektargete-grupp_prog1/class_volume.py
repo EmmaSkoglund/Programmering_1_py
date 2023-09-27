5ad7aba9e0e51ef359330c64d37350a408d54946
EmmaSkoglund <emma_skoglund@outlook.com>
2023-09-27 16:55:07 +0200

    arbete

--- Projektargete-grupp_prog1/class_volume.py
+++ Projektargete-grupp_prog1/class_volume.py
@@ -1,20 +1,26 @@
 import math
+
 class Volume:
 
     # Funktion för att beräkna volymen av en kub
-    def kub_volym(s):
-        volym = s ** 3
+    @staticmethod
+    def kub_volym(sida):
+        sida = float(sida)
+        volym = sida ** 3
         return volym
 
     # Funktion för att beräkna volymen av ett rätblock (rektangelbaserat prisma)
+    @staticmethod
     def rectangular_block_volym(L, B, H):
         volym = L * B * H
         return volym
 
     # Funktion för att beräkna volymen av ett prisma
+    @staticmethod
     def prism_volym(A, H):
         return A * H
 
     # Funktion för att beräkna volymen av en cylinder
+    @staticmethod
     def cylinder_volym(r, H):
         return math.pi * r ** 2 * H
