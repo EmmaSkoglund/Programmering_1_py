567443b841f21ec82cbb3b40116e2fee6edbdde4
EmmaSkoglund <emma_skoglund@outlook.com>
2023-09-27 22:38:12 +0200

    fortsatt projektarbete

--- Projektargete-grupp_prog1/class_volume.py
+++ Projektargete-grupp_prog1/class_volume.py
@@ -11,16 +11,26 @@ class Volume:
 
     # Funktion för att beräkna volymen av ett rätblock (rektangelbaserat prisma)
     @staticmethod
-    def rectangular_block_volym(L, B, H):
-        volym = L * B * H
+    def rectangular_block_volym(längd, bredd, höjd):
+        längd = float(längd)
+        bredd = float(bredd)
+        höjd = float(höjd)
+        volym = längd * bredd * höjd
         return volym
 
     # Funktion för att beräkna volymen av ett prisma
     @staticmethod
-    def prism_volym(A, H):
-        return A * H
+    def prism_volym(längd, bredd, höjd):
+        längd = float(längd)
+        bredd = float(bredd)
+        höjd = float(höjd)
+        volym = (längd*bredd) * höjd
+        return volym
 
     # Funktion för att beräkna volymen av en cylinder
     @staticmethod
-    def cylinder_volym(r, H):
-        return math.pi * r ** 2 * H
+    def cylinder_volym(radie, höjd):
+        radie = float(radie)
+        höjd = float(höjd)
+        volym = math.pi * radie**2 * höjd
+        return volym
